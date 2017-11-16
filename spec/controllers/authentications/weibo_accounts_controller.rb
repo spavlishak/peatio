0af44fe822a5e2b466f921dfc48cef6275a7325d
Dmitry Koval <dkoval@heliostech.fr>
2017-11-16 13:38:40 +0200

    Fix lots of tests

--- spec/controllers/authentications/weibo_accounts_controller.rb
+++ spec/controllers/authentications/weibo_accounts_controller.rb
@@ -1,13 +1,11 @@
-require 'spec_helper'
-
 module Authentications
   describe WeiboAccountsController, type: :controller do
     let(:member) { create(:member, email: nil, activated: false) }
     before { session[:member_id] = member.id }
 
     describe 'DELETE destroy' do
-      let!(:authentication) { create(:authentication, provider: 'weibo', member_id: member.id)}
-      subject(:do_request) { delete :destroy}
+      let!(:authentication) { create(:authentication, provider: 'weibo', member_id: member.id) }
+      subject(:do_request) { delete :destroy }
       context 'Only one authentication ' do
         it 'should not remove the authentication' do
           expect do
@@ -17,12 +15,12 @@ module Authentications
 
         it 'should tell user the reason' do
           do_request
-          expect(flash[:alert]).to eq t('authentications.weibo.destroy.last_auth_alert')
+          expect(flash[:alert]).to eq I18n.t('authentications.weibo.destroy.last_auth_alert')
         end
       end
 
       context 'More than one authentications' do
-        let!(:auth_ideneity) { create(:authentication, provider: 'identity', member_id: member.id)}
+        let!(:auth_ideneity) { create(:authentication, provider: 'identity', member_id: member.id) }
 
         it 'should delete the weibo authentication' do
           expect do
@@ -32,7 +30,7 @@ module Authentications
 
         it 'should set the flash message' do
           do_request
-          expect(flash[:notice]).to eq t('authentications.weibo.destroy.unbind_success')
+          expect(flash[:notice]).to eq I18n.t('authentications.weibo.destroy.unbind_success')
         end
       end
 
